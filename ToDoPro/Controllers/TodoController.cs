14985588daf4da3581aa85396baf9a8e6f81721b
Mayada-Ahmed <mayadaa.ahmed2810@gmail.com>
2024-12-24 21:16:25 +0200

    To Do Item

---
+++ ToDoPro/Controllers/TodoController.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoPro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        readonly ITodoItem _todoItem;
+        public TodoController(ITodoItem todoItem)
+        {
+            _todoItem = todoItem;
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(TodoItem newItem)
+        {
+             _todoItem.Create(newItem);
+            return Ok(newItem);
+        }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return Ok(_todoItem.GetAll());
+        }
+
+        [HttpGet("pending")]
+        public ActionResult<List<TodoItem>> GetPendingItem()
+        {
+            return Ok(_todoItem.GetPending());
+        }
+
+        [HttpPut("{id}/Complete")]
+        public ActionResult todoComplete(int id)
+        {
+                _todoItem.Update(id);
+                return Ok();
+        }
+
+    }
+}
